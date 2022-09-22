20d66b54d7c261f90f11f6236b6baec6ca73c1d8
Sebastian Pineda <63763483+sebascamilo12@users.noreply.github.com>
2022-09-22 11:32:26 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
+
 @NgModule({
   declarations: [
     AppComponent
+        
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
